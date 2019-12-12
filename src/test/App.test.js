c0e386c11ee33f6f0c5f23c96e638fe31cf48daa
lozzo27 <daniele.sordi@gmail.com>
2019-12-12 09:54:40 +0000

    restructuring folders

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../frontend/containers/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
